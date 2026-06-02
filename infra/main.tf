3991c8ae348caa093b37aa5a6e43fa3815f3ab8b
AngelEvaristo <62860453+AngelEvaristo@users.noreply.github.com>
2026-06-01 21:03:05 -0500

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -9,12 +9,10 @@ resource "aws_s3_bucket" "app_bucket" {
 
 resource "aws_s3_bucket_public_access_block" "app_bucket" {
   bucket = aws_s3_bucket.app_bucket.id  
-
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true 
-
 }
 
 resource "aws_s3_bucket_versioning" "app_bucket" {
